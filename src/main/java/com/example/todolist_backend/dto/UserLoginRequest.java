a073eb26e73bd74837de7672a476f7aabd8f2593
mlulucky <dmswjdans@gmail.com>
2023-08-14 16:53:23 +0900

    Jwt 실행 시 에러 발생 방지 의존성 및 코드 추가

--- src/main/java/com/example/todolist_backend/dto/UserLoginRequest.java
+++ src/main/java/com/example/todolist_backend/dto/UserLoginRequest.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor // 기본생성자 // Type definition error: [simple type, class com.example.todolist_backend.dto.UserLoginRequest] 에러 해결
 public class UserLoginRequest {
     private String account;
     private String password;
