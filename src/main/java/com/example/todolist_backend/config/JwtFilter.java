929df9a5920547a0adde350d66ddfe1d0df743fb
mlulucky <dmswjdans@gmail.com>
2023-08-16 16:35:28 +0900

    jwt 토큰 유효성 적용

--- src/main/java/com/example/todolist_backend/config/JwtFilter.java
+++ src/main/java/com/example/todolist_backend/config/JwtFilter.java
@@ -1,12 +1,15 @@
 package com.example.todolist_backend.config;
 
 import com.example.todolist_backend.service.UserService;
+import com.example.todolist_backend.utils.JwtTokenUtil;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,18 +18,39 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
-
+@Slf4j
 @RequiredArgsConstructor
-public class JwtFilter extends OncePerRequestFilter { // 토큰이 있는지 체크
+public class JwtFilter extends OncePerRequestFilter { // 🌈 토큰이 있는지 체크
 
     private final UserService userService;
-    private final String key;
+    private final String secretKey;
 
     // 🌈 doFilterInternal - jwt 권한 부여
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authroization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authroization : {}", authroization);
+
+        // Token 안보내면
+        if(authroization == null || !authroization.startsWith("Bearer ")) {
+            log.error("Authroization 을 잘못 보냈습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authroization.split(" ")[1]; // "Bearer 토큰
+
+        // Token Expired 되었는지 여부
+        if(JwtTokenUtil.isExpired(token, secretKey)) {
+            log.error("Token이 만료 되었습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         // account Token 에서 꺼내기
-        String account = "";
+        String account = JwtTokenUtil.getUserName(token, secretKey);
+        log.info("account : {}", account);
+
         // 권한 부여 //  사용자의 계정, 비밀번호 (null로 지정), 그리고 권한을 설정, 여기서는 "USER" 권한을 할당
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(account, null, List.of(new SimpleGrantedAuthority("USER")));
         // Detail
--- src/main/java/com/example/todolist_backend/utils/JwtTokenUtil.java
+++ src/main/java/com/example/todolist_backend/utils/JwtTokenUtil.java
@@ -11,7 +11,16 @@ import java.util.Date;
 // 토큰을 만들어내는 것 자체는 비즈니스 로직과 관련이 없기 때문에 util 패키지에 들어가는 것
 // 특정 개념과 독립적인 기능 + 다른 부분과 의존성이 없고 input parameter 만 갖고 단순한 처리만 하는 메소드들은 특히 정적(static) 메소드로 많이 구성
 public class JwtTokenUtil { // 🌈 userService 에서 로그인시 jwt 토큰 발행에 사용
-    public static String createToken(String account, String key, long expireTimeMs) {
+    public static String getUserName(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().get("account", String.class); // claimName 을 가져올때 String 타입으로 가져오겠다
+    }
+
+    public static boolean isExpired(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token) // setSigningKey() : 토큰 생성 비밀키 설정 // .parseClaimsJwt() : 주어진 토큰(token)을 분석. JWT 에 포함된 정보를 가져옴
+                .getBody().getExpiration().before(new Date()); //  JWT 토큰의 만료 시간을 가져와서 현재 시간과 비교
+    }
+    public static String createToken(String account, String secretKey, long expireTimeMs) {
         Claims claims = Jwts.claims(); // 정보를 담는 // 일종의 map
         claims.put("account", account);
 
@@ -19,7 +28,7 @@ public class JwtTokenUtil { // 🌈 userService 에서 로그인시 jwt 토큰 �
                 .setClaims(claims) // claims 지정
                 .setIssuedAt(new Date(System.currentTimeMillis())) // 만든 날짜
                 .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs)) // 만료 닐짜
-                .signWith(SignatureAlgorithm.HS256, key) // signWith(알고리즘방식, key) : key 암호화
+                .signWith(SignatureAlgorithm.HS256, secretKey) // signWith(알고리즘방식, key) : key 암호화
                 .compact();
     }
 }
