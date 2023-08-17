122f9b3467ce6ab392f678f8ae969df1353ae424
mlulucky <dmswjdans@gmail.com>
2023-08-18 07:48:45 +0900

    @repository 어노테이션 추가 및 계정유무 메서드 추가

--- src/main/java/com/example/todolist_backend/repository/UserRepository.java
+++ src/main/java/com/example/todolist_backend/repository/UserRepository.java
@@ -2,12 +2,15 @@ package com.example.todolist_backend.repository;
 
 import com.example.todolist_backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Repository // 스프링부트가 데이터베이스에 접근할 수 있도록 도와주는 컴포넌트 스캔 어노테이션
+public interface UserRepository extends JpaRepository<User, Long> { // JpaRepository<엔티티, 엔티티 pk 타입> : 엔티티를 매개체로 데이터베이스와 통신
     Optional<User> findByUserName(String userName);
     Optional<User> findByAccount(String account);
+    boolean existsByAccount(String account);
 
 
 }
