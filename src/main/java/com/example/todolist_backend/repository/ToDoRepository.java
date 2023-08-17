cb07dea24a61de3c6f5f55ba5a2a57475d7d1afe
mlulucky <dmswjdans@gmail.com>
2023-08-18 07:47:58 +0900

    @repository 어노테이션 추가

--- src/main/java/com/example/todolist_backend/repository/ToDoRepository.java
+++ src/main/java/com/example/todolist_backend/repository/ToDoRepository.java
@@ -2,7 +2,9 @@ package com.example.todolist_backend.repository;
 
 import com.example.todolist_backend.domain.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
 
 }
