cc4a80793335b9f970f0336acf61953dea9b71e9
mlulucky <dmswjdans@gmail.com>
2023-08-18 17:59:02 +0900

    Token 생성 클래스 작업중

---
+++ src/main/java/com/example/todolist_backend/service/TokenProvider.java
@@ -0,0 +1,15 @@
+package com.example.todolist_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenProvider {
+    private static final String SECURITY_KEY = "jwtSecretKey";
+
+    // jwt 토큰 생성
+
+    // jwt 토큰 복호화
+
+
+
+}
