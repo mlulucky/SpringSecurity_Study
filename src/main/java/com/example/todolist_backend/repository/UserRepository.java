a6e7f24e88dacb7d546c537c32dd93e0db1e9335
mlulucky <dmswjdans@gmail.com>
2023-08-21 07:47:34 +0900

    feat : UserRepository 에 계정 존재여부, 계정패스워드 존재여부 확인 메서드 추가

--- src/main/java/com/example/todolist_backend/repository/UserRepository.java
+++ src/main/java/com/example/todolist_backend/repository/UserRepository.java
@@ -2,12 +2,14 @@ package com.example.todolist_backend.repository;
 
 import com.example.todolist_backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
     Optional<User> findByAccount(String account);
-
-
+    boolean existsByAccountAndPassword(String account, String password);
+    boolean existsByAccount(String account);
 }
