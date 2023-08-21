9a38f91ec26f56802b53d697a95a7fd9d3f1ecb1
mlulucky <dmswjdans@gmail.com>
2023-08-21 19:14:56 +0900

    fix: ToDoController 스프링시큐리티 테스트

--- src/main/java/com/example/todolist_backend/controller/ToDoController.java
+++ src/main/java/com/example/todolist_backend/controller/ToDoController.java
@@ -4,11 +4,8 @@ import com.example.todolist_backend.dto.todo.ToDoCreateRequest;
 import com.example.todolist_backend.dto.todo.ToDoCreateResponse;
 import com.example.todolist_backend.service.ToDoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/todo")
@@ -21,6 +18,11 @@ public class ToDoController {
         // toDoService.createToDo(toDoCreateRequest);
         // return ResponseEntity.ok().body("투두 등록이 완료 되었습니다.");
         return ResponseEntity.ok().body(toDoService.createToDo(toDoCreateRequest));
+    }
+
+    @GetMapping("/")
+    public String getToDo(@AuthenticationPrincipal String account) { // jwtFilter 에 SecurityContext 에 인증할 객체로 account 를 담았기때문에, account 정보를 가져와 쓸수있다.
+        return "로그인된 사용자는 " + account + "입니다.";
     }
 
 }
