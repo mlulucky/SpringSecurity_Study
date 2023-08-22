365c5708a80ade77adc31182c86c50ffabc0b22b
mlulucky <dmswjdans@gmail.com>
2023-08-22 17:52:07 +0900

    feature: 투두 서비스 list 추가 및 컨트롤러, repository 등 적용

--- src/main/java/com/example/todolist_backend/controller/ToDoController.java
+++ src/main/java/com/example/todolist_backend/controller/ToDoController.java
@@ -1,5 +1,6 @@
 package com.example.todolist_backend.controller;
 
+import com.example.todolist_backend.domain.ToDo;
 import com.example.todolist_backend.dto.todo.ToDoCreateRequest;
 import com.example.todolist_backend.dto.todo.ToDoCreateResponse;
 import com.example.todolist_backend.service.ToDoService;
@@ -7,17 +8,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/todo")
 @RequiredArgsConstructor
 public class ToDoController {
     private final ToDoService toDoService;
 
-    @PostMapping("{id}")
-    public ResponseEntity<ToDoCreateResponse> writeToDo(@RequestBody ToDoCreateRequest toDoCreateRequest) {
+    @PostMapping("/{uId}/todos")
+    public ResponseEntity<ToDoCreateResponse> register(@PathVariable Long uId, @RequestBody ToDoCreateRequest toDoCreateRequest) {
         // toDoService.createToDo(toDoCreateRequest);
         // return ResponseEntity.ok().body("투두 등록이 완료 되었습니다.");
+
         return ResponseEntity.ok().body(toDoService.createToDo(toDoCreateRequest));
+    }
+
+    @GetMapping("/{uId}/list")
+    public ResponseEntity<List<ToDo>> list(@PathVariable Long uId) {
+        return ResponseEntity.ok().body(toDoService.list(uId));
+
     }
 
     @GetMapping("/")
