0bb790c590ba5013a5604b4e3b4971b09d327322
mlulucky <dmswjdans@gmail.com>
2023-08-23 17:56:11 +0900

    feat: 유저로그인시 응답 유저데이터(UserLoginData) 별도 생성 및 UserLoginResponse 데이터에서 유저 User 를 UserLoginData 로 변경

---
+++ src/main/java/com/example/todolist_backend/dto/user/UserLoginData.java
@@ -0,0 +1,15 @@
+package com.example.todolist_backend.dto.user;
+
+import com.example.todolist_backend.domain.ToDo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class UserLoginData {
+    private Long id;
+    private String account;
+    private String userName;
+}
--- src/main/java/com/example/todolist_backend/dto/user/UserLoginResponse.java
+++ src/main/java/com/example/todolist_backend/dto/user/UserLoginResponse.java
@@ -1,9 +1,12 @@
 package com.example.todolist_backend.dto.user;
 
+import com.example.todolist_backend.domain.ToDo;
 import com.example.todolist_backend.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,5 +14,6 @@ import lombok.NoArgsConstructor;
 public class UserLoginResponse { // 유저 로그인 시 서버에서 응답하는 DTO
     private String token;
     private int experTime;
-    private User user;
+    private UserLoginData user;
+
 }
