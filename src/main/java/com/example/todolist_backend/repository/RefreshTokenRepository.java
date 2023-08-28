9c3d67977a0c43d8951f48579feb9e144dd4f57d
mlulucky <dmswjdans@gmail.com>
2023-08-28 17:59:26 +0900

    feat: refresh 토큰 JPA repository 생성

---
+++ src/main/java/com/example/todolist_backend/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.todolist_backend.repository;
+
+import com.example.todolist_backend.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserIdAndReissueCountLessThan(Long id, long count);
+
+}
