fe1558c989bcad14c0c150f0bbd6a1d521de5863
mlulucky <dmswjdans@gmail.com>
2023-08-28 09:31:58 +0900

    feat : userId 멤버변수 추가

--- src/main/java/com/example/todolist_backend/dto/todo/ToDoDTO.java
+++ src/main/java/com/example/todolist_backend/dto/todo/ToDoDTO.java
@@ -12,12 +12,14 @@ public class ToDoDTO {
     private Long id;
     private String content;
     private boolean done;
+     private Long userId;
 
     public static ToDoDTO convertDTO(ToDo todo) {
         return new ToDoDTO(
                 todo.getId(),
                 todo.getContent(),
-                todo.isDone()
+                todo.isDone(),
+                todo.getUser() != null ? todo.getUser().getId() : null
         );
     }
 
