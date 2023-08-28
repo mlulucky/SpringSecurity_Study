86019637e1ed20229cfd2daa7bbbd2ff56667967
mlulucky <dmswjdans@gmail.com>
2023-08-28 17:58:59 +0900

    feat: 로그인 응답 데이터에 refresh 토큰 추가

--- src/main/java/com/example/todolist_backend/dto/user/UserLoginResponse.java
+++ src/main/java/com/example/todolist_backend/dto/user/UserLoginResponse.java
@@ -12,8 +12,9 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 public class UserLoginResponse { // 유저 로그인 시 서버에서 응답하는 DTO
-    private String token;
+    private String token; // accessToken
     private int experTime;
     private UserLoginData user;
+    private String refreshToken; // 리프레시토큰
 
 }
