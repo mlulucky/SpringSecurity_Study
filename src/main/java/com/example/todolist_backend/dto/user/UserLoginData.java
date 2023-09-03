f2c0bfb2214cefacf3fea72e1f12f3e4043ad9b0
mlulucky <dmswjdans@gmail.com>
2023-09-04 07:50:18 +0900

    refactor : 주석삭제

--- src/main/java/com/example/todolist_backend/dto/user/UserLoginData.java
+++ src/main/java/com/example/todolist_backend/dto/user/UserLoginData.java
@@ -1,16 +1,13 @@
 package com.example.todolist_backend.dto.user;
 
-import com.example.todolist_backend.domain.ToDo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public class UserLoginData {
-    // private int id;
-     private Long id;
+    private Long id;
     private String account;
     private String userName;
 }
